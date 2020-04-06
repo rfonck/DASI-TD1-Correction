ce9048ce68a69e3a748f2bc313ff2a925e8eece3
rfonck <rfonck@outlook.fr>
2020-04-06 15:25:21 +0200

    Projet initial
    voilà l'état actuel des choses

---
+++ target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Client_.java
@@ -0,0 +1,17 @@
+package fr.insalyon.dasi.metier.modele;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-22T18:32:06")
+@StaticMetamodel(ClientOld.class)
+public class Client_ { 
+
+    public static volatile SingularAttribute<ClientOld, String> motDePasse;
+    public static volatile SingularAttribute<ClientOld, String> mail;
+    public static volatile SingularAttribute<ClientOld, Long> id;
+    public static volatile SingularAttribute<ClientOld, String> nom;
+    public static volatile SingularAttribute<ClientOld, String> prenom;
+
+}
