1cfc64674947aa8e673b97c4853d54f10fb62524
rfonck <rfonck@outlook.fr>
2020-04-16 11:05:13 +0200

    goiubgpi ^

--- target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Astrologue_.java
+++ target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Astrologue_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-16T08:13:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-16T11:01:55")
 @StaticMetamodel(Astrologue.class)
 public class Astrologue_ extends Medium_ {
 
